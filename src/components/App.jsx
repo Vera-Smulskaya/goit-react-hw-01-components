84f7588a70377590603ad435cfb585c7d08d1f6d
Vira Smulska <95082786+Vera-Smulskaya@users.noreply.github.com>
2023-10-11 18:45:26 +0300

    add data.json and create Profile component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,9 @@
-import user from 'path/to/user.json';
-import { Profile } from './profile/Profile';
+import user from '../components/data/user.json';
+import Profile from './profile/Profile';
 
 export const App = () => {
   return (
-    <div
-    // style={{
-    //   height: '100vh',
-    //   display: 'flex',
-    //   justifyContent: 'center',
-    //   alignItems: 'center',
-    //   fontSize: 40,
-    //   color: '#010101',
-    // }}
-    >
-      {/* React homework template */}
+    <div>
       <Profile
         username={user.username}
         tag={user.tag}
