f2396371741e4f70ac9767b9c28c0dd293948d73
Vira Smulska <95082786+Vera-Smulskaya@users.noreply.github.com>
2023-10-19 22:00:53 +0300

    add styles to Statistics component

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -1,18 +1,32 @@
+import css from './Statistics.module.css';
+
 export default function Statistics({ title, stats }) {
   return (
-    <section className="statistics">
-      {title && <h2 className="title">{title}</h2>}
+    <section className={css.statistics}>
+      {title && <h2 className={css.statTitle}>{title}</h2>}
 
-      <ul className="stat-list">
+      <ul className={css.statList}>
         {stats.map(el => {
           return (
-            <li key={el.id} className="item">
-              <span className="label">{el.label}</span>
-              <span className="percentage">{el.percentage}</span>
+            <li
+              key={el.id}
+              className={css.statListItem}
+              style={{
+                backgroundColor: getRandomHexColor(),
+              }}
+            >
+              <span className={css.statListLabel}>{el.label}</span>
+              <span className={css.statListPercentage}>{el.percentage}%</span>
             </li>
           );
         })}
       </ul>
     </section>
   );
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
