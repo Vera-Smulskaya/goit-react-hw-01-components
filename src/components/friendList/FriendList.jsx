e84e17e41355841288bc3c0f5186e287cf1949e5
Vira Smulska <95082786+Vera-Smulskaya@users.noreply.github.com>
2023-10-19 22:33:02 +0300

    add styles to FriendsList and FriendsListItem components

--- src/components/friendList/FriendList.jsx
+++ src/components/friendList/FriendList.jsx
@@ -1,8 +1,9 @@
 import FriendListItem from 'components/friendListItem/FriendListItem';
+import css from './FriendsList.module.css';
 
 export default function FriendList({ friends }) {
   return (
-    <ul className="friend-list">
+    <ul className={css.friendsList}>
       {friends.map(({ avatar, name, isOnline, id }) => {
         return (
           <FriendListItem
--- src/components/friendListItem/FriendListItem.jsx
+++ src/components/friendListItem/FriendListItem.jsx
@@ -1,10 +1,19 @@
+import css from './FriendListItem.module.css';
+
 export default function FriendListItem(props) {
   const { avatar, name, isOnline, id } = props;
+  const isStatusColor = isOnline ? 'green' : 'red';
+
   return (
-    <li className="item" key={id}>
-      <span className="status">{isOnline}</span>
-      <img className="avatar" src={avatar} alt={name} width="200" />
-      <p className="name">{name}</p>
+    <li className={css.friendsItem} key={id}>
+      <span
+        className={css.friendsStatus}
+        style={{ backgroundColor: isStatusColor }}
+      >
+        {isOnline}
+      </span>
+      <img className={css.friendsAvatar} src={avatar} alt={name} width="200" />
+      <p className={css.friendsName}>{name}</p>
     </li>
   );
 }
